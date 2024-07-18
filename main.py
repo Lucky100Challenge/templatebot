0d2dbb9dba2f5147df5cb16f242232a35479163b
That Notion Dude <thatnotiondude@gmail.com>
2024-07-17 20:28:34 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,86 @@
+import time
+import streamlit as st
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
+import os, sys, json
+from fpdf import FPDF
+
+# Load environment va   riables
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+
+st.set_page_config(
+    page_title="NotionHelper - Your AI Notion Assistant",
+    page_icon=":robot:",
+    layout="wide",
+)
+
+
+def ai_sales_coach(user_input):
+    if not user_input:
+        return "Please provide a valid question or request."
+    elif "help" in user_input:
+        return "I'm here to help you with any questions you have about Notion. How can I assist you today?"
+    else:
+    
+        prompt = f"""
+        I would like you to act as a professional web developer; your job will be to design notion widgets out of HTML, CSS, and JavaScript. 
+        I require each code to be in separate files. 
+        We will be using them to embed within a notion page
+      
+        
+        Please provide a comprehensive response to the following request:
+        {user_input}
+        """
+        llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
+        return llm.invoke(prompt)
+
+
+# UI Title
+st.markdown("## NotionHelper - Your AI Notion Widgets Assistant")
+st.markdown("### Ask me anything about Notion and I will help you with the best possible answer!")
+st.markdown("---")  # Horizontal line
+    
+    
+# Don't show Chat History
+if "messages" not in st.session_state:
+    st.session_state.messages = []  # Initialize chat history
+    #clear chat history
+    st.session_state.messages = []
+    # Welcome message
+    st.session_state.messages.append({"role": "assistant", "content": "Welcome! Type a message to get started."})
+
+
+# Display chat messages
+with st.container():  # Use container for styling
+    for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+                
+# User Input
+if prompt := st.chat_input("Your message"):
+    # Append user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    
+    # Display user message
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # Display "Sales Coach is typing..."
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty() 
+        message_placeholder.markdown("Assstiant is typing...")
+
+    # Get and append AI response (with a delay to simulate typing)
+    time.sleep(1)  # Adjust the delay as needed
+    response = ai_sales_coach(prompt)
+    message_placeholder.markdown(response)  # Update the placeholder
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    
+    # Clear user input after sending message
+    st.session_state.messages = st.session_state.messages[-100:]  # Limit chat history to last 100 messages
+    
+    
+  
