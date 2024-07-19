f8acc270ec3d852262c5193e2cb050c3c112b03a
MinimalistCodes <thatnotiondude@gmail.com>
2024-07-18 19:04:51 -0500

    copy

--- pages/MarketingCopy.py
+++ pages/MarketingCopy.py
@@ -1,11 +1,15 @@
+import time
 import streamlit as st
-import google_bard
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
+import os, sys, json
+import subprocess 
 
-# Load environment variables
+# Load environment va   riables
 load_dotenv()
 
 api_key = os.getenv("GOOGLE_API_KEY")
+
 
 st.set_page_config(
     page_title="NotionHelper - Your AI Notion Assistant",
@@ -13,42 +17,53 @@ st.set_page_config(
     layout="wide",
 )
 
+
 def ai_sales_coach(user_input):
     if not user_input:
         return "Please provide a valid question or request."
     elif "help" in user_input:
         return "I'm here to help you with any questions you have about Notion. How can I assist you today?"
     else:
+    
         prompt = f"""
+        You will act as a professional marketer, and copywriter.
+        I will tell you about my project and its features, and then you will write me a few tweets that I can use to market it.
+        The tweets should be engaging and designed to attract attention.
+        
+        Please provide a comprehensive response to the following request:
         {user_input}
         """
-        llm = google_bard.generate_text(prompt, api_key)
-        return llm
+        llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=api_key)
+        return llm.invoke(prompt)
+
+
+# UI Title
+with st.expander("Instructions"):
+    st.markdown("### Instructions")
+    st.markdown(
+        """
+        1. Tell me about your product and it's features.
+        2. Type your message in the chat box below and press Enter.
+        3. I will provide you with a few tweets for your product.",
+        """
+    )
     
 with st.sidebar:
     #clear chat history
     if st.button("Clear Chat History"):
         st.session_state.messages = []
+        st.session_state.messages.append({"role": "assistant", "content": "Chat history cleared."})
         
-# Don't show Chat History
-st.markdown(
-    """
-    <style>
-        .st-eb {
-            display: none;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
 
+# Don't show Chat History
 if "messages" not in st.session_state:
     st.session_state.messages = []  # Initialize chat history
     #clear chat history
     st.session_state.messages = []
     # Welcome message
     st.session_state.messages.append({"role": "assistant", "content": "Welcome! Type a message to get started."})
-    
+
+
 # Display chat messages
 with st.container():  # Use container for styling
     for message in st.session_state.messages:
@@ -63,18 +78,20 @@ if prompt := st.chat_input("Your message"):
     # Display user message
     with st.chat_message("user"):
         st.markdown(prompt)
-        
-    # Get response from AI
+
+    # Display "Sales Coach is typing..."
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty() 
+        message_placeholder.markdown("Assstiant is typing...")
+
+    # Get and append AI response (with a delay to simulate typing)
+    time.sleep(1)  # Adjust the delay as needed
     response = ai_sales_coach(prompt)
-    
-    # Append AI response to chat history
+    message_placeholder.markdown(response)  # Update the placeholder
     st.session_state.messages.append({"role": "assistant", "content": response})
     
-    # Display AI response
-    with st.chat_message("assistant"):
-        st.markdown(response)
-        
-# Clear user input after sending message
-st.session_state.messages = st.session_state.messages[-100:]  # Limit chat history to last 100 messages
-        
-        
+    # Clear user input after sending message
+    st.session_state.messages = st.session_state.messages[-100:]  # Limit chat history to last 100 messages
+    
+    
+  
